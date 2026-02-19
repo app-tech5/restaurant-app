a97df7344281a316646532220aaaea3245a44ce8
nass <cabliveer@gmail.com>
2026-02-19 12:36:44 +0100

    Delete comments from estlint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -58,46 +58,45 @@ module.exports = [
       "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],
       "no-console": ["warn", { allow: ["warn", "error"] }],
       'no-comments/disallowComments': 'error',
-
-      // i18next rules for detecting hardcoded strings
+      
       'i18next/no-literal-string': [
         'error',
         {
-          // Allow strings that are likely not meant to be translated
+          
           ignore: [
-            // Empty strings
+            
             '',
-            // Single characters
+            
             /^[a-zA-Z]$/,
-            // Common non-translatable strings
+            
             /^[\d\s\.\-\+\=\<\>\[\]\(\)\{\}\*\+\?\^\$\|\\]*$/,
-            // URLs and paths
+            
             /^https?:\/\//,
             /^\/\w+/,
-            // CSS classes and styles
+            
             /^[a-zA-Z][a-zA-Z0-9\-_]*$/,
-            // Import/export statements
+            
             /^(import|export|from|require)$/,
-            // React component names
+            
             /^[A-Z][a-zA-Z0-9]*$/,
-            // Common programming keywords
+            
             /^(true|false|null|undefined|function|const|let|var|if|else|for|while|do|switch|case|default|try|catch|finally|throw|return|break|continue)$/,
-            // File extensions
+            
             /\.(js|jsx|ts|tsx|json|css|scss|html|xml)$/,
-            // Common abbreviations
+            
             /^(id|url|api|http|https|www|com|org|net|io)$/i,
-            // Version numbers
+            
             /^\d+\.\d+\.\d+$/,
-            // Time formats
+            
             /^\d{1,2}:\d{2}$/,
-            // Date formats
+            
             /^\d{1,2}\/\d{1,2}\/\d{4}$/,
-            // Phone numbers (basic pattern)
+            
             /^\+?\d[\d\s\-\(\)]+$/,
-            // Email domains
+            
             /^@\w+\.\w+$/,
           ],
-          // Allow JSX attributes that commonly contain non-translatable strings
+          
           ignoreCallee: [
             'console.log',
             'console.error',
@@ -107,7 +106,7 @@ module.exports = [
             'require',
             'import',
           ],
-          // Allow specific JSX attributes
+          
           ignoreAttribute: [
             'key',
             'testID',
